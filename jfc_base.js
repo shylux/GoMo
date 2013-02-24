344b9c81217b578c76cea4d95632eec2bc5c51b0
Shylux <shylux@gmail.com>
2013-02-24 17:29:51 +0100

    Revert "added display of field data. text inside of the table breaks layout."
    This reverts commit 0308590b311a49af5a4bb73fe173aa5eb827d258.

--- jfc_base.js
+++ jfc_base.js
@@ -6,30 +6,3 @@ function resize() {
   div.css('height', div.width());
   $('.card').css('height', $('.card').width()*1.5);
 }
-
-function updateGameState(data) {
-	for (x=0; x < data.board.length ; x++) {
-		for (y=0; y < data.board.length; y++) {
-			var datafield = data.board[y][x];
-      var domfield = $('#board').find('[y="'+y+'"]').find('[x="'+x+'"]');
-      domfield.addClass('color_' + datafield.color);
-      domfield.html('player: '+datafield.player+'<br/>level: '+datafield.level);
-    }
-  }
-  //TODO
-}
-function showError(error_string) {
-  //TODO create nice error window
-  alert(error_string);
-}
-
-$(window).ready(function() {
-  // add x and y coordinates to tr and td to identify them later on
-  $('#board tr').each(function(y, value) {
-    $(this).attr('y', y);
-    $(this).find('td').each(function(x, value) {
-      $(this).attr('x', x);
-    });
-  });
-  GoMo.connect(updateGameState, showError);
-});
